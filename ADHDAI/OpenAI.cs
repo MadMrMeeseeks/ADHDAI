8b2fe02e00a41bb99483cc8379e60106a41a4aa4
MadMrMeeseeks <95554942+MadMrMeeseeks@users.noreply.github.com>
2023-05-30 12:49:22 -0400

    API Key removed

--- ADHDAI/OpenAI.cs
+++ ADHDAI/OpenAI.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,12 +33,10 @@ class OpenAI
     static public async Task<string> DataParser(string rawUserInput)
     {
 
-        string openAIKey = "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";
+        string openAIKey = openAItestAPI;
         //string openAIKey = "";
-
         string url = "https://api.openai.com/v1/engines/text-davinci-003/completions";
-
-        string aiGuidance = "Take unstructured user entries. Break the information down into the following categories: To-Do, Goal. Project and Calendar Event. To-Dos are one off tasks which may be recurring and can be due on any day for example: “Pick up prescription” would be a to-do. Goals are something the user wants to accomplish,for example “I am having trouble quitting sugar and need help” The goal would be to quit eating sugar. Projects are things the user wants to build or create, for example “I want to renovate my backyard” would be an example of a project. Finally, a Calendar Event is something that needs to be added to the user's calendar, such as dinner plans, meetings and other common calendar worthy events. For the TIME column, Calendar events should be formatted in a 24 time format (for example 6:00pm should be 18:00) and are required to have a time. The other events can be NA, TODAY, or and actual time depending on the case. Please convert the table to the corresponding JSON format ";
+        string aiGuidance = "Take unstructured user entries.Break the information down into the following categories: To - Do, Goal/Project and Calendar Event. To - Dos are one off tasks which may be recurring and can be due on any day for example: “Pick up prescription” would be a to-do. Goals are something the user wants to accomplish,for example “I am having trouble quitting sugar and need help” The goal would be to quit eating sugar.Projects are things the user wants to build or create, for example “I want to renovate my backyard” would be an example of a project.Finally, a Calendar Event is something that needs to be added to the user's calendar, such as dinner plans, meetings and other common calendar worthy events. For the TIME column, Calendar events should be formatted in a 24 time format (for example 6:00pm should be 18:00) and are required to have a time. The other events can be NA, TODAY, or and actual time depending on the case. Please convert the table to the corresponding JSON format. These should be generated from scratch each time and should not include information from previous runs of this model. All text outputted must be JSON compliant.";
 
         string jsonExample = """
             {
@@ -46,17 +48,12 @@ class OpenAI
                 }
               ]
             }
-            """;
-
-        var prompt = $"{aiGuidance}\n\n{rawUserInput}\n\n{jsonExample}";
-
-
-
-
+            """
+            ;
+            var prompt = $"{aiGuidance}\n\n{rawUserInput}\n\n{jsonExample}";
         using (var httpClient = new HttpClient())
         {
             httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {openAIKey}");
-
             var requestData = new
             {
                 prompt = prompt,
@@ -66,14 +63,11 @@ class OpenAI
                 frequency_penalty = 0,
                 presence_penalty = 0
             };
-
             var jsonRequestData = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
             var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
-
             using (var response = await httpClient.PostAsync(url, content))
             {
                 response.EnsureSuccessStatusCode();
-
                 var jsonResponseData = await response.Content.ReadAsStringAsync();
                 var responseObject = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonResponseData);
                 string completionText = responseObject.choices[0].text;
